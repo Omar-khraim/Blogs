eca7dc7593a6fe96d489fa0432b55310cc81002a
Omar-khraim <oaskhraim@gmail.com>
2023-05-31 23:58:54 -0400

    1-added the users controller and implemented the users service 2-added model mapper dependecy 3-implemented the list mapper so it can be used with all classes

--- src/main/java/com/example/blogs/Services/UsersService.java
+++ src/main/java/com/example/blogs/Services/UsersService.java
@@ -3,9 +3,13 @@ package com.example.blogs.Services;
 import com.example.blogs.Domain.DTOs.UsersDTO;
 import com.example.blogs.Domain.Entity.Users;
 
+import java.util.List;
+
 public interface UsersService {
 
     UsersDTO createUser(Users user);
     UsersDTO findUserByID(long userId);
     void deleteUserById(long userId);
+
+    List<UsersDTO> findALlUsers();
 }
